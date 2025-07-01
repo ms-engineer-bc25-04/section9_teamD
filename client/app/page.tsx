2de0b542a9068026650c53664dc170b8684914f7
108wre <ritanaka1117@gmail.com>
2025-07-01 17:57:36 +0900

    clientファイル削除

--- client/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthCard } from "@/components/auth-card"
-
-export default function Page() {
-  return (
-    <div className="min-h-screen flex items-center justify-center p-4">
-      <AuthCard />
-    </div>
-  )
-}
