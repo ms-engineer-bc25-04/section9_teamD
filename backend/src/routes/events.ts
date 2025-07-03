9cc68ae793166fab49f8c32d2f2bb0157fce401e
e-ozk22 <o.kre101201.m@gmail.com>
2025-07-03 22:47:57 +0900

    ルーティング変更

--- backend/src/routes/events.ts
+++ backend/src/routes/events.ts
@@ -1,9 +1,29 @@
+// import { Router } from "express";
+// import {
+//   createEvent,
+//   getEvent,
+//   updateEvent,
+// } from "../controllers/event-controller";
+
+// const router = Router();
+
+// router.post("/events", createEvent);
+// router.put("/events/:id", updateEvent);
+// router.get("/events/:id", getEvent);
+
+// export default router;
+
 import { Router } from "express";
 import { createEvent, updateEvent } from "../controllers/event-controller";
 
 const router = Router();
 
-router.post("/events", createEvent);
-router.put("/events/:id", updateEvent);
+router.post("/events", (req, res, next) => {
+  createEvent(req, res).catch(next);
+});
+
+router.put("/events/:id", (req, res, next) => {
+  updateEvent(req, res).catch(next);
+});
 
 export default router;
